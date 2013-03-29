3ab69f62b71550b789d7c8a60344e636db2c7832
bmcorser <benmarshallcorser@gmail.com>
2013-03-29 17:43:11 +0000

    nicer

--- venvdiff.sh
+++ venvdiff.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-source `which virtualenvwrapper_lazy.sh`
+source $(which virtualenvwrapper_lazy.sh)
 
 function rm_freezes {
   rm $pip_freezes
@@ -14,7 +14,7 @@ do
   if [[ $? > 0 ]]; then
     exit 1
   else
-    pip_freezes[i]=`mktemp /tmp/venvdiff.XXX`
+    pip_freezes[i]=$(mktemp /tmp/venvdiff.XXX)
     trap rm_freezes 1 2 3 15
     echo "#$venv\n" > $pip_freezes[i]
     pip freeze >> $pip_freezes[i]
