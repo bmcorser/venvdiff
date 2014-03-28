89faa8056e147bf7864847b8b3cb1bab60cf0088
bmcorser <benmarshallcorser@gmail.com>
2014-03-28 17:27:44 +0000

    Be less dumb, make it faster, make it work on my Mac

--- venvdiff.sh
+++ venvdiff.sh
@@ -10,15 +10,13 @@ function rm_freezes {
 for (( i = 1; i <= ${#@}; i++ ))
 do
   venv=${@[i]}
-  workon $venv
   if [[ $? > 0 ]]; then
     exit 1
   else
     pip_freezes[i]=$(mktemp /tmp/venvdiff.XXX)
     trap rm_freezes 1 2 3 15
     echo "#$venv\n" > $pip_freezes[i]
-    pip freeze >> $pip_freezes[i]
-    deactivate
+    ~/.virtualenvs/$venv/bin/pip freeze >> $pip_freezes[i]
   fi
 done
 
